9525f2993607a02a4e399d018d17f5ab4680f52c
+ <+>
2022-09-05 22:09:13 +0300

    updating seed.rb

--- db/seed.rb
+++ db/seed.rb
@@ -2,4 +2,32 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
+Car.create([
+    {
+      year: "2016",
+      model: "city-experess",
+      make: "chevrolet"
+    },
+    {
+        year: "2016",
+        model: "a3",
+        make: "audi"
+    },
+    {
+        year: "2016",
+        model: "x4",
+        make: "bmw"
+    },
+    {
+        year: "2016",
+        model: "alfa-romeo",
+        make: "4c"
+    },
+    {
+        year: "2016",
+        model: "db9-gt",
+        make: "aston-martin"
+    }
+  ])
+
 puts "✅ Done seeding!"
