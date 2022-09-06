e7c16fd7d231fd57e87212ae695348167ca5043c
+ <+>
2022-09-06 08:26:37 +0300

    deleting seed

--- db/seed.rb
+++ db/seed.rb
@@ -2,32 +2,5 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
-Car.create([
-    {
-      year: "2016",
-      model: "city-experess",
-      make: "chevrolet"
-    },
-    {
-        year: "2016",
-        model: "a3",
-        make: "audi"
-    },
-    {
-        year: "2016",
-        model: "x4",
-        make: "bmw"
-    },
-    {
-        year: "2016",
-        model: "alfa-romeo",
-        make: "4c"
-    },
-    {
-        year: "2016",
-        model: "db9-gt",
-        make: "aston-martin"
-    }
-  ])
 
 puts "✅ Done seeding!"
